53843c6b88b6be4e08eb26ac73c2f8f6dfe45b04
Fidel Adrian Sanchez Bravo <adrianbra07@gmail.com>
2018-04-12 08:56:18 -0500

    Practica Examen

--- app/src/main/java/mx/ipn/cecyt9/examen_primerparcial_fideladriansanchez/MainActivity.java
+++ app/src/main/java/mx/ipn/cecyt9/examen_primerparcial_fideladriansanchez/MainActivity.java
@@ -2,12 +2,37 @@ package mx.ipn.cecyt9.examen_primerparcial_fideladriansanchez;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String Usuario = "Alexis";
+    String Password = "1234";
+    String UsuarioObtenido = "";
+    String PasswordObtenido = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Validacion(View view) {
+
+        EditText tv = findViewById(R.id.Usuario);
+        UsuarioObtenido = tv.getText().toString();
+
+        EditText tv2 = findViewById(R.id.Contra);
+        PasswordObtenido = tv2.getText().toString();
+
+        if(PasswordObtenido.equals(Password) && UsuarioObtenido.equals(Usuario)){
+            Toast.makeText(MainActivity.this,UsuarioObtenido,Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(MainActivity.this,UsuarioObtenido,Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
